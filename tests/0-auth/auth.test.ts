c4f201f6eb84f8f0a6ccc5af9aa117b3ad7a44a3
Harshit Chouhan <harshit.chouhan@asmadiya.com>
2023-03-04 11:36:04 +0530

    test cases done

--- tests/0-auth/auth.test.ts
+++ tests/0-auth/auth.test.ts
@@ -1,8 +1,19 @@
 import chai from 'chai';
 import chaiHttp from 'chai-http';
 
-export let token = '';
-
 chai.should();
 chai.use(chaiHttp);
 
+describe('Currency Rates', () => {
+  describe('GET /currency/rates', () => {
+    it('It should get currency rates for today', (done) => {
+      chai
+        .request(`http://localhost:4200`)
+        .get(`/curreny-conversion/api/v1/currency/rates`)
+        .end((err, response) => {
+          response.body.should.have.property('message').to.equal('success');
+          done();
+        });
+    });
+  });
+});
