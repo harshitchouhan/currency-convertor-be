ca4e6b96806ff7146186e8b77c650deef826a8a0
Harshit Chouhan <harshit.chouhan@asmadiya.com>
2023-03-04 14:24:19 +0530

    UAT env removed

--- src/database/database.config.ts
+++ src/database/database.config.ts
@@ -1,28 +1,16 @@
 import { EncryptionAndDecryption } from '../core/Encryption&Decryption';
 
-export const DB_ENVIRONMENT: 'DEV' | 'UAT' | 'PROD' = 'DEV';
+export const DB_ENVIRONMENT: 'DEV' | 'PROD' = 'DEV';
 
 export const DB_CONFIGS: any = {
   DEV: {
     POSTGRESS: {
       'currency-convertor-db': {
-        user: EncryptionAndDecryption.decryption('U2FsdGVkX19rqxKPjN1jrtUK8cjdnBr2TRjRCkN/DE4='),
-        host: 'localhost',
-        database: EncryptionAndDecryption.decryption('U2FsdGVkX1+otnCctZemJqelGMdb81AuoiGY5J982j60sU/9sCr/ohYfHud2CCkg'),
-        password: EncryptionAndDecryption.decryption('U2FsdGVkX19fZ5rZMWeeS9oaJX0ROpDhy9NkUHJxPNM='),
-        port: EncryptionAndDecryption.decryption('U2FsdGVkX1+TbsnBdYUSSQHMZCkKRMUXyB/3wRX7c8o='),
-      },
-    },
-  },
-
-  UAT: {
-    POSTGRESS: {
-      'currency-convertor-db': {
-        user: EncryptionAndDecryption.decryption('U2FsdGVkX19rqxKPjN1jrtUK8cjdnBr2TRjRCkN/DE4='),
-        host: 'localhost',
-        database: EncryptionAndDecryption.decryption('U2FsdGVkX1+otnCctZemJqelGMdb81AuoiGY5J982j60sU/9sCr/ohYfHud2CCkg'),
-        password: EncryptionAndDecryption.decryption('U2FsdGVkX19fZ5rZMWeeS9oaJX0ROpDhy9NkUHJxPNM='),
-        port: EncryptionAndDecryption.decryption('U2FsdGVkX1+TbsnBdYUSSQHMZCkKRMUXyB/3wRX7c8o='),
+        user: 'ubuntu',
+        host: '65.2.78.173',
+        database: 'currency-convertor-db',
+        password: 'password',
+        port: 5432,
       },
     },
   },
